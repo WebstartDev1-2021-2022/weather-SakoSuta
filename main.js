f913801685963a4773fd6a8f152975822e5c2145
Emilie <emiliemontpre@outlook.com>
2022-03-08 10:11:35 +0100

    Update

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 // TODO: Récupérer les éléments du DOM dans des constantes 
 
 // Constant
-const tempImg = document.querySelector("tempImg")
+const tempImg = document.querySelector(".tempImg")
 const lieu = document.querySelector(".lieu")
 const temp = document.querySelector(".temp")
+const heure = document.querySelectorAll(".heure")
+const jour = document.querySelectorAll(".jour")
 
 // Requête fetch
 const apiKey = "3fd72506573bbc8268962948af289955"
@@ -18,30 +20,52 @@ const getWeatherOf = async (position) =>
 {
     try
     {
-        let lat = position.coords.latitude;
-        let lon = position.coords.longitude;
+        const {latitude, longitude} = position.coords
 
-        const res = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely&units=metric&lang=fr&appid=${apiKey}`)
-        const data = await res.json()
+        const allPromise = Promise.all([
+            fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely&units=metric&lang=fr&appid=${apiKey}`),
+            fetch(`https://api-adresse.data.gouv.fr/reverse/?lon=${longitude}&lat=${latitude}`)
+        ])
 
-        // console.log(data)
-        updateUI(data)
+        const results = await allPromise
+        const weatherData = await results[0].json()
+        const cityData = await results[1].json()
 
-        temp.innerText = `${Math.trunc(data.current.temp)}°`
+        // Temps Actuel
+        temp.innerText = `${Math.trunc(weatherData.current.temp)}°`
+        lieu.innerText = `${cityData.features[0].properties.city}`
+        tempImg.src = `img/${weatherData.current.weather[0].icon}.svg`
+
+        // Temps par Heure
+        for(let th=0; th < 24; th++)
+        {
+            heure[th].innerHTML = `<p class="tempH">${new Date(weatherData.hourly[th].dt * 1000).getHours()}h</p>
+                                  <img src="img/${weatherData.hourly[th].weather[0].icon}.svg" alt="Temps_par_heure">
+                                  <p class="tempH">${Math.trunc(weatherData.hourly[th].temp)}°</p>`
+        }
+
+        let week = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi']
+
+        for(let tj=0; tj < 5; tj++)
+        {
+            let dt_day = new Date(weatherData.daily[tj].dt * 1000).getDay()
+            jour[tj].innerHTML = `<p class="tempSJ">
+                                    ${week[dt_day]}
+                                  </p>
+                                  <img src="img/${weatherData.daily[tj].weather[0].icon}.svg" alt="Temps_par_jour">
+                                  <p class="tempSMM">
+                                  Min : ${Math.trunc(weatherData.daily[tj].temp.min)}
+                                  ° | 
+                                  Max : ${Math.trunc(weatherData.daily[tj].temp.max)}°</p>`
+        }
+
+        console.log(weatherData,cityData);
     } 
     catch(error)
     {
         console.error('Erreur dans le getWeatheerOf() ~>', error);
     }
 }
-
-const updateUI = (data) => 
-{
-    console.log(data);
-}
-
-const TempsActuel
-
 
 // Récupérer la géolocalisation de l'utilisateur et faire la requ^te à l'API
 navigator.geolocation.getCurrentPosition( 
